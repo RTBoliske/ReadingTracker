a0f6c150207d2a4c20a6a4af06f34a2160db439e
Aaron Tyler <aaron.tyler@techelevator.com>
2018-08-09 16:23:39 -0400

    Capstone Seed

---
+++ Capstone.Web/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Common.WebHost;
+
+namespace Capstone.Web
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+
+            string connectionString = ConfigurationManager.ConnectionStrings["DB"].ConnectionString;
+
+
+            //kernel.Bind<ISomething>().To<Something>().WithConstructorArgument("connectionString", connectionString);
+
+            return kernel;
+
+        }
+    }
+}
