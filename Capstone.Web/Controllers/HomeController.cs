06dfe94602e5c775d7d648490d3f1c13df46558f
Austin Delarosa <austin.delarosa@gmail.com>
2018-08-16 09:58:19 -0400

    Fixed integration testing for creating a new user

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -163,8 +163,8 @@ namespace Capstone.Web.Controllers
                 user.LastName = model.LastName;
                 user.Username = model.Username;
                 user.Password = ph.Hash;
-                user.FamilyName = ((User)Session["User"]).FamilyName;
-                //user.FamilyID = _db.GetFamilyID(model.FamilyID);
+                user.FamilyName = _db.GetFamilyFromFamilyID(model.FamilyID);
+                user.FamilyID = ((User)Session["User"]).FamilyID;
                 user.Salt = ph.Salt;
                 user.RoleID = model.RoleID;
 
