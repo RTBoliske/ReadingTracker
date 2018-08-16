15b49c74933e901556890c86db9830543f83fc15
Austin Delarosa <austin.delarosa@gmail.com>
2018-08-16 15:09:48 -0400

    Modified database saltiness

--- Capstone.Web/DatabaseScripts/data.sql
+++ Capstone.Web/DatabaseScripts/data.sql
@@ -12,10 +12,10 @@ INSERT INTO Users (First_name, Last_name, FamilyID, Username, Password, Salt, Ro
 VALUES ('Austin', 'DeLaRosa', 1, 'adelarosa', 'FD7PS/mVScRRZ8jZXcg3xkOA8EE=', 'KlJGgDgFj8iaio+x1Bdp/Q==', 2);
 
 INSERT INTO Users (First_name, Last_name, FamilyID, Username, Password, Salt, RoleID) 
-VALUES ('Domingo', 'Lopez', 4, 'dlopez', '1234asdf', 'dlopez', 2);
+VALUES ('Joseph', 'DeLaRosa', 4, 'jdelarosa', '9G0jGNMY2SjFAM4phOKNQElVOpE=', 'GjOVi5qyrpaFfLEwfnzJlQ==', 3);
 
 INSERT INTO Users (First_name, Last_name, FamilyID, Username, Password, Salt, RoleID) 
-VALUES ('Ted', 'Boliske', 2, 'tboliske', '1234asdf', 'tboliske', 2);
+VALUES ('Ted', 'Boliske', 2, 'tboliske', 'cQgdTq37FO2vDD+jCr4mhFWBePc=', 'VBZ1VOmoZ08HQ5PJOUHMxw==', 2);
 
 INSERT INTO Users (First_name, Last_name, FamilyID, Username, Password, Salt, RoleID) 
-VALUES ('Joe', 'Binegar', 3, 'jbinegar', '1234asdf', 'jbinegar', 2);
+VALUES ('Joe', 'Binegar', 3, 'jbinegar', 'ujf3h3kIuzWmJtr8Zti7YoPECN8=', 'bkkKEisi+ah7yJYD8s4tBg==', 2);
