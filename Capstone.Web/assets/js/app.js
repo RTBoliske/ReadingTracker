f8cbb554c6fb394efe5943f6323fe00dcfb942c6
Ted Boliske <tboliske@cinci.rr.com>
2018-08-22 16:32:35 -0400

    moved login/register to navbar. updated nav icons, updated images to display families reading, removed image for stats, removed footer image, updated copyright text

--- Capstone.Web/assets/js/app.js
+++ Capstone.Web/assets/js/app.js
@@ -29,39 +29,39 @@ jQuery(document).ready(function(){
         $('body').addClass('loaded');
     })
 
-  /*======== 2. MENU SCROLL ========*/
-   $(window).load(function(){
-    $('.body-wrapper').each(function(){
-      var header_area = $('.header-wrapper');
-      var main_area = header_area.children('.navbar');
+  ///*======== 2. MENU SCROLL ========*/
+  // $(window).load(function(){
+  //  $('.body-wrapper').each(function(){
+  //    var header_area = $('.header-wrapper');
+  //    var main_area = header_area.children('.navbar');
 
-      var logo = main_area.find('.navbar-header');
-      var navigation = main_area.find('.navbar-collapse');
-      var original = {
-        nav_top: navigation.css('margin-top')
-      };
+  //    var logo = main_area.find('.navbar-header');
+  //    var navigation = main_area.find('.navbar-collapse');
+  //    var original = {
+  //      nav_top: navigation.css('margin-top')
+  //    };
 
-      $(window).scroll(function(){
-        if( main_area.hasClass('bb-fixed-header') && ($(this).scrollTop() === 0 || $(this).width() < 750)){
-          main_area.removeClass('bb-fixed-header').appendTo(header_area);
-          navigation.animate({'margin-top': original.nav_top}, {duration: 300, queue: false, complete: function(){
-            header_area.css('height', 'auto');
-          }});
-        }else if( !main_area.hasClass('bb-fixed-header') && $(this).width() > 750 &&
-          $(this).scrollTop() > header_area.offset().top + header_area.height() - parseInt($('html').css('margin-top')) ){
+  //    $(window).scroll(function(){
+  //      if( main_area.hasClass('bb-fixed-header') && ($(this).scrollTop() === 0 || $(this).width() < 750)){
+  //        main_area.removeClass('bb-fixed-header').appendTo(header_area);
+  //        navigation.animate({'margin-top': original.nav_top}, {duration: 300, queue: false, complete: function(){
+  //          header_area.css('height', 'auto');
+  //        }});
+  //      }else if( !main_area.hasClass('bb-fixed-header') && $(this).width() > 750 &&
+  //        $(this).scrollTop() > header_area.offset().top + header_area.height() - parseInt($('html').css('margin-top')) ){
 
-          header_area.css('height', header_area.height());
-          main_area.css({'opacity': '0'}).addClass('bb-fixed-header');
-          main_area.appendTo($('body')).animate({'opacity': 1});
+  //        header_area.css('height', header_area.height());
+  //        main_area.css({'opacity': '0'}).addClass('bb-fixed-header');
+  //        main_area.appendTo($('body')).animate({'opacity': 1});
 
-          navigation.css({'margin-top': '0px'});
-        }
-      });
-    });
+  //        navigation.css({'margin-top': '0px'});
+  //      }
+  //    });
+  //  });
 
-    $(window).trigger('resize');
-    $(window).trigger('scroll');
-  });
+  //  $(window).trigger('resize');
+  //  $(window).trigger('scroll');
+  //});
 
 /*======== 3. SELECT BOX ========*/
   $('.select-drop').selectbox();
